24c6728bab2d242572dd3d81dbcdaf43b6e0b84f
Afiq Harith <afiqharith05@gmail.com>
2021-04-04 04:29:30 +0800

    new

--- Circle_Solver.cpp
+++ Circle_Solver.cpp
@@ -17,10 +17,11 @@ using std::ifstream;
 using std::ofstream;
 using std::string;
 
-const double centroidA = 256;
-const double centroidB = 256;
-const double new_centroidA = 232;
-const double new_centroidB = 320;
+const int centroidA = 256;
+const int centroidB = 256;
+const int new_centroidA = 232;
+const int new_centroidB = 320;
+
 const double pi = 3.14159265;
 const int SIZE = 100;
 
@@ -33,36 +34,48 @@ double *ptrnew_y = new_y;
 
 class Calculation
 {
+private:
+    int centroid_A;
+    int centroid_B;
+    int new_centroid_A;
+    int new_centroid_B;
 
 public:
-    // Calculate radius
-    double radius(double x, double y, double centroidA, double centroidB)
+    Calculation(int centroidA, int centroidB, int new_centroidA, int new_centroidB)
     {
-        return sqrt(pow((x - centroidA), 2) + pow((y - centroidA), 2));
+        centroid_A = centroidA;
+        centroid_B = centroidB;
+        new_centroid_A = new_centroidA;
+        new_centroid_B = new_centroidB;
+    }
+    // Calculate radius
+    double radius(double x, double y)
+    {
+        return sqrt(pow((x - centroid_A), 2) + pow((y - centroid_A), 2));
     }
 
     // Calculate angle for x (cos x)
-    double radian_x(double radius_val, double x, double centroidA)
+    double radian_x(double radius_val, double x)
     {
-        return acos((x - centroidA) / radius_val);
+        return acos((x - centroid_A) / radius_val);
     }
 
     // Calculate angle of y (sin y)
-    double radian_y(double radius_val, double y, double centroidA)
+    double radian_y(double radius_val, double y)
     {
-        return asin((y - centroidA) / radius_val);
+        return asin((y - centroid_A) / radius_val);
     }
 
     // Get new x value
-    double get_new_x(double radius_val, double new_centroidA, double angle_x)
+    double get_new_x(double radius_val, double angle_x)
     {
-        return (radius_val * cos(angle_x)) + new_centroidA;
+        return (radius_val * cos(angle_x)) + new_centroid_A;
     }
 
     // Get new y value
-    double get_new_y(double radius_val, double new_centroidB, double angle_y)
+    double get_new_y(double radius_val, double angle_y)
     {
-        return (radius_val * sin(angle_y)) + new_centroidB;
+        return (radius_val * sin(angle_y)) + new_centroid_B;
     }
 
 } * cal;
@@ -84,21 +97,20 @@ int main()
     ofstream writexy("./data/processed-data/new_xy.csv");
     ofstream write_angle_deg("./data/processed-data/angle_in_degree.csv");
 
-    cal = new Calculation;
+    cal = new Calculation(centroidA, centroidB, new_centroidA, new_centroidB);
 
     for (index = 0; index < SIZE; index++)
     {
-        read >> x[index];
-        read >> y[index];
+        read >> x[index] >> y[index];
 
         // Question 1
-        radius_val[index] = cal->radius(x[index], y[index], centroidA, centroidB);
-        angle_x[index] = cal->radian_x(radius_val[index], x[index], centroidA);
-        angle_y[index] = cal->radian_y(radius_val[index], y[index], centroidB);
+        radius_val[index] = cal->radius(x[index], y[index]);
+        angle_x[index] = cal->radian_x(radius_val[index], x[index]);
+        angle_y[index] = cal->radian_y(radius_val[index], y[index]);
 
         // Question 2
-        new_x[index] = cal->get_new_x(radius_val[index], new_centroidA, angle_x[index]);
-        new_y[index] = cal->get_new_y(radius_val[index], new_centroidB, angle_y[index]);
+        new_x[index] = cal->get_new_x(radius_val[index], angle_x[index]);
+        new_y[index] = cal->get_new_y(radius_val[index], angle_y[index]);
 
         // writexy << new_x[index] << "," << new_y[index] << endl;
         writexy << *(ptrnew_x + index) << "," << *(ptrnew_y + index) << std::endl;
